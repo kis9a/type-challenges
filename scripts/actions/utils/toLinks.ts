a7dc0ed521d8cee16bb158c010a2d1a3b32d4d6f
kis9a <kis9ax@gmail.com>
2021-02-10 18:15:36 +0900

    reduce extra files to play in local

--- scripts/actions/utils/toLinks.ts
+++
@@ -1,8 +0,0 @@
-import { Quiz } from '../../types'
-import { SupportedLocale, t } from '../../locales'
-import { toReadmeShort } from '../../toUrl'
-
-export function toLinks(quiz: Quiz, locale: SupportedLocale) {
-  return '\n\n'
-        + `> ${t(locale, 'link.view-on-github')}${toReadmeShort(quiz.no, locale)}`
-}
